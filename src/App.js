9d0e50df2780a007433bbc2c85b1ada9658ee4cd
alejandrolemos20 <alejandro.lemos7@hotmail.com>
2022-04-13 16:08:39 -0500

    Mi primer React

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 
 function App() {
   return (
+    
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <h1>Mi primer react</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -18,6 +20,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <h1>hola mundo</h1>
     </div>
   );
 }
